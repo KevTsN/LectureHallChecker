3ce70b8b0d54cc7bb249328aab28c5504c5076fc
Kevin <kevin3ngeleka@gmail.com>
2024-11-12 09:48:25 -0500

    Added in next class option, and edited readme

--- README.md
+++ README.md
@@ -2,13 +2,25 @@
 
 This Python script checks what classes are happening inside Carleton University's lecture halls at the time the script runs. <br>
 Download the **monthly** iCalendar files from the lecture halls you would like to use, then add them to the ScheduleFiles folder.<br>
-Link to lecture halls: https://booking.carleton.ca/index.php?p=RoomSearch&r=1 <br>
+[Link to lecture hall schedules](https://booking.carleton.ca/index.php?p=RoomSearch&r=1) <br>
 
 ## Necessary Installations:
 >**PiP is required**
 
 If you don't have PiP in your environment, : <br>
-Download *get-pip.py* from [text](https://bootstrap.pypa.io/get-pip.py)
-Run "*python get-pip.py*"
-
+Download *get-pip.py* from [Bootstrap get-pip.py](https://bootstrap.pypa.io/get-pip.py)<br>
+Run "*python get-pip.py*"<br>
+<r>
 After that runs, run "*pip install -r requirements.txt*" in your environment terminal.
+
+### Running the program:
+Run *python checkSchedules.py* in your terminal.
+
+### Setting Favourites:
+*Favourites.txt* is a text file that holds one of your favourite courses on each line. These courses will show "FAVOURITE" abvoe them when the program runs if they are shown.<br>
+Run *python checkSchedules.py fav* in your terminal, and you will be shown your current favourite courses (from *Favourites.txt*). You will then be prompted to add new favourites, which will be added to Favourites.txt. The program will then run as normal, listing current courses/times.
+
+### Viewing next lecture:
+Run *python checkSchedules.py next* in your terminal, and the program will print out the next class that will happen in the lecture hall, in addition to the current one happening.
+
+>The system arguments "next" and "fav" can both be used at the same time.
--- checkSchedules.py
+++ checkSchedules.py
@@ -21,15 +21,16 @@ if __name__ == "__main__":
     for fav in ff:
         fav = fav.split('\n', 1)[0]
         favouritesList.insert(len(favouritesList), fav)
+    
+    if("fav" in sys.argv):
 
-    if(len(sys.argv) > 1 and sys.argv[1] == "fav"):
         #inserting favourites before running program
         newFavsList = []
         print("FAVOURITES LIST: ")
         for fav in favouritesList:
             print(fav)
         
-        print("Enter in favourite coruse, or type \"q\" or \"quit\" to exit")
+        print("Enter in favourite course, or type \"q\" or \"quit\" to exit")
         newFav = "";
 
         while(newFav != "q" and newFav != "quit"):
@@ -60,13 +61,19 @@ if __name__ == "__main__":
     #ok done 
     favouritesSet = set(favouritesList)
     
+    
     for path in ics_pths:
+        nextDict = {}
+        print("Hall: " + path.stem + "\n")
         with path.open() as f:
             calendar = icalendar.Calendar.from_ical(f.read())
-
         for event in calendar.walk('VEVENT'):
             startDt = event['DTSTART'].dt
             endDt = event['DTEND'].dt
+        
+            startDiff = int( (startDt - today).total_seconds() / 60)
+
+
             if startDt <= today <= endDt:
             
                 summary = str(event.get("SUMMARY"))
@@ -91,5 +98,37 @@ if __name__ == "__main__":
                 print("They started at {fst}, which was {fsm} minutes ago.".format(fst=startTime, fsm = minStart))
                 print("They will end at {fet}, which is {fem} minutes from now.\n".format(fet = endTime, fem = minEnd))
                                                                             
-                                                                    
+            #handling next courses
+            elif ("next" in sys.argv and startDiff < 180 and startDiff>0):
 
+
+                summary = str(event.get("SUMMARY"))
+                summary = summary.split(">", 1)
+                toSplit = summary[-1].split("<",1)[0]
+                toSplit = toSplit.split("-")
+                course = toSplit[-1]
+
+                if(course[-1]=="F" or course[-1]=="W"):
+                    course = course.split(" ")[0]
+                
+                endTime = endDt.time().strftime("%I:%M %p")
+                startTime = startDt.time().strftime("%I:%M %p")
+                minToStart = int( (startDt-today).total_seconds() / 60 )
+
+                nextDict[startDiff] = {"course":course, "endTime":endTime, "startTime": startTime, "minToStart": minToStart}
+        
+        
+        #find absolute next course (smallest time diff)
+        nextDict = dict(sorted(nextDict.items()))
+
+        if (len(nextDict)>0):
+            nextCourse = next(iter(nextDict.items())) #returns 0=key, 1=value /dict
+            nextCourse = nextCourse[1] #redefine as the value, which is a dict
+
+            print("{courseName} is the next class in {hallName}".format(courseName=nextCourse["course"], hallName = path.stem))
+
+            if(nextCourse["course"] in favouritesSet):
+                    print("FAVOURITE");
+            
+            print("They will start at {fst}, which is {fsm} from now.".format(fst=nextCourse["startTime"], fsm = nextCourse["minToStart"]))
+            print("They will end at {fet}.\n".format(fet = nextCourse["endTime"]))
